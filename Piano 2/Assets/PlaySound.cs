3f767b29b28c564b7283fedc3440094e57587bbc
Sam Van Horn <svanhorn@bodaboda.org>
2015-04-12 02:25:10 -0400

    Add the real Piano

---
+++ Piano 2/Assets/PlaySound.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySound : MonoBehaviour {
+
+	public float semitone_offset = 0;
+	public int collisions = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown () {
+		PlayNote ();
+	}
+
+	void PlayNote () {
+		GetComponent<AudioSource> ().pitch = Mathf.Pow (2f, semitone_offset/12.0f);
+		GetComponent<AudioSource> ().Play();
+	}
+	
+}
