4076563e5a093fe22ed85ddb498b2da5915d895c
tbohanno <tbohanno@gmail.com>
2015-04-11 21:19:15 -0400

    messing around with diferent sensor locations

--- Unity/Assets/keyscript.cs
+++ Unity/Assets/keyscript.cs
@@ -13,7 +13,20 @@ public class keyscript : MonoBehaviour {
 	
 	}
 
-	void OnMouseDown(){
-		GetComponent<AudioSource>().Play();
+	private bool IsHand(Collider other)
+	{
+		if (other.transform.parent && other.transform.parent.parent &&     other.transform.parent.parent.GetComponent<HandModel>())
+			return true;
+		else
+			return false;
+	}
+	
+	void OnTriggerEnter(Collider other) 
+	{
+		if (IsHand(other))
+		{
+			Debug.Log("A hand collided!");
+			audio.Play();
+		}  
 	}
 }
